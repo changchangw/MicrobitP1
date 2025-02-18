b29a6957cc5ad161ff0754d4c5a391450a18d5fc
changchangw <474038188@qq.com>
2025-02-18 13:00:29 +0000

    add sound

--- main.ts
+++ main.ts
@@ -1,4 +1,14 @@
+serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+    receivedData = serial.readUntil(serial.delimiters(Delimiters.NewLine))
+    if (receivedData == "CRASH") {
+        music.startMelody(music.builtInMelody(Melodies.Wedding), MelodyOptions.Once)
+        basic.showIcon(IconNames.Ghost)
+        basic.pause(1000)
+        basic.clearScreen()
+    }
+})
 let xAccel = 0
+let receivedData = ""
 serial.redirectToUSB()
 basic.forever(function () {
     xAccel = input.acceleration(Dimension.X)
